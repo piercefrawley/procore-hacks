7bfe97188d28dc0f742476e22befba280ed3ccce
Pierce Frawley <pierce@procore.com>
2017-01-19 10:45:15 -0800

    Base component, router, and provider

--- client/js/index.js
+++ client/js/index.js
@@ -1,7 +1,31 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { IndexRoute, Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+import { Base } from './components';
 
-$(function () {ReactDOM.render(
-  (<div>Hello React</div>),
-  document.querySelector('#content')
-)});
+// Add the reducer to your store on the `routing` key
+const store = createStore(
+  combineReducers({
+    routing: routerReducer,
+  })
+);
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
+$(function () {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router history={history}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Base}/>
+        </Route>
+      </Router>
+    </Provider>,
+    document.querySelector('#root')
+  )
+});
