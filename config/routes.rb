b1784785f4af150e1c113cb08f7ae50a422815cd
Gino Briones <gino@procore.com>
2017-01-19 14:20:20 -0800

    All html requests will go to a static html page, api requests will be nested under /api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: "static#root"
-  get '/*path' => 'static#root'
+  get '*path' => 'static#root', constraints: -> (request) do
+    !request.xhr? && request.format.html?
+  end
+  scope :api, defaults: { format: 'json' } do
+    resources :cost_codes
+  end
 end
