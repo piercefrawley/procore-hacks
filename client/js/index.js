123c20f12d68258dd3d02f8765436310f2ac6ea0
Pierce Frawley <pierce@procore.com>
2017-01-19 09:38:22 -0800

    Add client node environment

---
+++ client/js/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+$(function () {ReactDOM.render(
+  (<div>Hello React</div>),
+  document.querySelector('#content')
+)});
