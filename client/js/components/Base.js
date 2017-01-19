da4050f06b5f109b9d0b75793ad4fe99432df020
Pierce Frawley <pierce@procore.com>
2017-01-19 15:46:44 -0800

    Connect module

--- client/js/components/Base.js
+++ client/js/components/Base.js
@@ -1,12 +1,16 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+import { connectModule } from 'redux-modules';
+import module from '../redux/modules/app';
 
-export default class Base extends Component {
+class Base extends Component {
   render() {
+    console.log(this.props);
     return (
       <div className="flex-container">
         <div>Hello React</div>
       </div>
-    )
+    );
   }
 }
+
+export default connectModule(module)(Base);
