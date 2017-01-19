fdb766ca04971c1c6881f3c97e9db33b51351473
Pierce Frawley <pierce@procore.com>
2017-01-19 11:33:12 -0800

    Redux logger

--- client/js/App.js
+++ client/js/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import '../scss/main.scss'
 
 class App extends Component {
   render() {
--- client/js/index.js
+++ client/js/index.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore, combineReducers } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { fromJS } from 'immutable';
+import createLogger from 'redux-logger';
 import { Provider } from 'react-redux';
 import { IndexRoute, Router, Route, browserHistory } from 'react-router';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import { Base } from './components';
-import '../scss/base.scss';
+
+const initialState = {};
+const emptyMiddleware = () => next => action => next(action);
+
+let logger = emptyMiddleware;
+logger = createLogger({
+  stateTransformer: object => fromJS(object).toJS(),
+  actionTransformer: object => fromJS(object).toJS(),
+  collapsed: true,
+  logErrors: false,
+});
 
 // Add the reducer to your store on the `routing` key
 const store = createStore(
   combineReducers({
     routing: routerReducer,
-  })
+  }),
+  initialState,
+  applyMiddleware(logger)
 );
 
 // Create an enhanced history that syncs navigation events with the store
