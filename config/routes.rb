55f2c44082aecb166eba9665174fa1ff95084c91
Yann Lebreton <yann.lebreton@procore.com>
2017-01-20 11:35:55 -0800

    companies_controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     !request.xhr? && request.format.html?
   end
   scope :api, defaults: { format: 'json' } do
+    resources :companies
     resources :cost_codes
     resources :projects
     resources :stats
